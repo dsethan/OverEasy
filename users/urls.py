66c1df89cdeb0b9c05ea9397ffd9d474c311ca57
Ethan Gottlieb <ethan@dormserv.com>
2014-10-20 13:54:36 -0400

    add logout correct link

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
 	url(r'^login', views.user_login, name='user_login'),
     url(r'^register', views.user_reg, name='user_reg'),
     url(r'^process_new_user', views.process_new_user, name='process_new_user'),
+    url(r'^logout', views.user_logout, name='user_logout'),
+
     )
