997d9a76396ebced78645d78397d96c53af11056
Ethan Gottlieb <ethan@dormserv.com>
2014-10-20 13:07:45 -0400

    new fields

--- demand/models.py
+++ demand/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Demand(models.Model):
 	first = models.CharField(max_length=30)
 	last = models.CharField(max_length=30)
-	email = models.CharField(max_length=30)
-	phone = models.IntegerField(max_length=12)
+	email = models.CharField(max_length=50)
+	phone = models.CharField(max_length=30)
 	address = models.CharField(max_length=200)
