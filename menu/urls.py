7586e176b4a6034fb32408a8f4ea7a34882c2c90
ehg17 <gottlieb.ethan@gmail.com>
2014-10-22 12:02:34 -0400

    cart

--- menu/urls.py
+++ menu/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^$', views.add_item_to_cart, name='add_item_to_cart'),
+	url(r'^display_menu/', views.display_menu, name='display_menu'),
 	url(r'^remove_from_cart/', views.remove_from_cart, name='remove_from_cart'),
 
     )
