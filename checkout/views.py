76177f43603a3c1a714372f1de27b0446e143b69
ehg17 <gottlieb.ethan@gmail.com>
2014-10-22 22:31:03 -0400

    checkout

--- checkout/views.py
+++ checkout/views.py
@@ -90,39 +90,6 @@ def process_new_card(request):
 	context = RequestContext(request)
 	user = request.user
 
-	try:
-	  # Use Stripe's bindings...
-	  pass
-	except stripe.error.CardError, e:
-	  # Since it's a decline, stripe.error.CardError will be caught
-	  body = e.json_body
-	  err  = body['error']
-
-	  print "Status is: %s" % e.http_status
-	  print "Type is: %s" % err['type']
-	  print "Code is: %s" % err['code']
-	  # param is '' in this case
-	  print "Param is: %s" % err['param']
-	  print "Message is: %s" % err['message']
-	except stripe.error.InvalidRequestError, e:
-	  # Invalid parameters were supplied to Stripe's API
-	  pass
-	except stripe.error.AuthenticationError, e:
-	  # Authentication with Stripe's API failed
-	  # (maybe you changed API keys recently)
-	  pass
-	except stripe.error.APIConnectionError, e:
-	  # Network communication with Stripe failed
-	  pass
-	except stripe.error.StripeError, e:
-	  # Display a very generic error to the user, and maybe send
-	  # yourself an email
-	  pass
-	except Exception, e:
-	  # Something else happened, completely unrelated to Stripe
-	  pass
-
-
 	if request.method == 'POST':
 		cart_id = request.POST.get('cart_id')
 		total_price = request.POST.get('total_price')
