9a31e574cf4a8afaad09766316bc5d2c294eae92
ehg17 <gottlieb.ethan@gmail.com>
2014-10-23 20:39:59 -0400

    messing with menu

--- referral/models.py
+++ referral/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Referral(models.Model):
+	initiator = models.ForeignKey(User)
+	target_email = models.CharField(max_length=50)
+	code = models.CharField(max_length=15)
+	active = models.BooleanField(default=True)
+	percent_discount = models.IntegerField(default=0)
+	dollar_discount = models.IntegerField(default=0)
+	
