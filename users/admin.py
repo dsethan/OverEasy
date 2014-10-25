a8b76c1f815ac1b04427f5c848c4c75699f57b68
dsethan <ethan@dormserv.com>
2014-10-25 11:01:05 -0400

    add driver/staff profiles to admin

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,10 @@ from users.models import UserProfile
 class UserProfileAdmin(admin.ModelAdmin):
 	list_display = ('user', 'address', 'city', 'state', 'phone')
 
+class DriverProfileAdmin(admin.ModelAdmin):
+	list_display = ('user', 'address', 'city', 'state', 'phone')
+
+class StaffProfileAdmin(admin.ModelAdmin):
+	list_display = ('user', 'address', 'city', 'state', 'phone')
+
 admin.site.register(UserProfile, UserProfileAdmin)
