19b915e70e3b703f3f7c23a7c4637ff2f083c1ab
dsethan <ethan@dormserv.com>
2014-10-30 20:17:15 -0400

    so it doesn't change on fridays

--- cal/views.py
+++ cal/views.py
@@ -25,7 +25,7 @@ def render_first_look_calendar(request):
 	weekday = today.isocalendar()[2]
 	mon = today - timedelta(days=weekday-1)
 
-	if (weekday == 5) or (weekday == 6) or (weekday == 7):
+	if (weekday == 6) or (weekday == 7):
 		week = week + 1
 		mon = mon + timedelta(days=7)
 
@@ -91,7 +91,7 @@ def render_second_look_calendar(request):
 	weekday = today.isocalendar()[2]
 	mon = today - timedelta(days=weekday-1)
 
-	if (weekday == 5) or (weekday == 6) or (weekday == 7):
+	if (weekday == 6) or (weekday == 7):
 		week = week + 1
 		mon = mon + timedelta(days=7)
 
